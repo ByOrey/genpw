9ca5c100d3ace9d33722fd6e046c9280735b28c7
Petrov Vladislav <ktoto22q@gmail.com>
2025-05-31 04:52:29 +0700

    Reliese symbols randomizer

--- src/libgenpw/genpw.c
+++ src/libgenpw/genpw.c
@@ -8,3 +8,11 @@ static const CharSets sets
            .lowercase = "abcdefghijklmnopqrstuvwxyz",
            .uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
            .special = "!@#$%^&*()-_=+[]{}|;:,.<>/?"};
+
+static int get_random_char(const char* charset)
+{
+    size_t len = strlen(charset);
+    if (len == 0)
+        return '\0';
+    return charset[rand() % len];
+}

f85350ff9ed0b0d2e8cb4c52cbb612a19f85824c
Petrov Vladislav <ktoto22q@gmail.com>
2025-05-31 04:51:35 +0700

    Create Symbols Sets

--- src/libgenpw/genpw.c
+++ src/libgenpw/genpw.c
@@ -2,3 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+
+static const CharSets sets
+        = {.digits = "0123456789",
+           .lowercase = "abcdefghijklmnopqrstuvwxyz",
+           .uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+           .special = "!@#$%^&*()-_=+[]{}|;:,.<>/?"};

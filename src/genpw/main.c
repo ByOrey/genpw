07aa4dc2ae6f701679e0f21e9752f515c92f7ff8
Petrov Vladislav <ktoto22q@gmail.com>
2025-05-31 06:57:27 +0700

    Fix include pach and .yml

--- src/genpw/main.c
+++ src/genpw/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 void genpw_info(const char* prog_name)
 {
--- src/libgenpw/genpw.c
+++ src/libgenpw/genpw.c
@@ -1,5 +1,6 @@
 #include "genpw.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
